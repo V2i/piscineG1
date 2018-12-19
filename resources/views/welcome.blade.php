9ee5573104ccd30ce05e2de78e98fd6c6e62d75c
V2i <43175508+V2i@users.noreply.github.com>
2018-12-19 11:07:21 +0100

    insertion de 2 layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,19 @@
 
 @section('title', 'Page Title')
 
+@section('searchbar')
+    @parent
+
+    <p>This is appended to the master searchbar.</p>
+@stop
+
+@section('content')
+    <p>This is my body content.</p>
+@stop
+@extends('layouts.sidebar')
+
+@section('title', 'Page Title')
+
 @section('sidebar')
     @parent
 
