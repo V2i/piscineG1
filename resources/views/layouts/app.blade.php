dfd79718c4344e550677421d52680b5de34ca282
V2i <43175508+V2i@users.noreply.github.com>
2018-12-25 17:28:42 +0100

    fusion navbar et bar de connexion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,17 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#"> Accueil <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tag</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Produit Soldé</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Panier</a>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,3 @@
-@extends('layouts.searchbar')
-
-@section('title', 'Page Title')
-
-@section('searchbar')
-    @parent
-
-    <p>This is appended to the master searchbar.</p>
-@stop
-
-@section('content')
-    <p>This is my body content.</p>
-@stop
 @extends('layouts.app')
 
 @section('content')
