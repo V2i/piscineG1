75f4febbfb8810350b34260ba6e20b0ff8b129df
nesrinech <barcelona-hoshi@hotmail.fr>
2019-01-08 00:26:05 +0100

    accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 </style>
 <div class="container-fluid jumbotron" style="background-image:url('http://vps627012.ovh.net/~piscine/piscine/resources/image/bandeau-montpellier.jpg'); margin-top: -40px;">
     <div class="container text-center text">
-        <h1 class="whitetext"> La CCI vous souhaite la bienvenue sur sa marketplace de l'Hérault </h1>
+        <h1 class="whitetext" style="background-color:hsla(0, 0%, 60%, 0.3)"> La CCI vous souhaite la bienvenue sur sa marketplace de l'Hérault </h1>
     </div>
 </div>
 <div class="container">
@@ -43,14 +43,14 @@
         </div> <!-- fin carousel -->
 
         <div style="padding-top: 10px"></div>
-        <h3 style="font-family: serif;">Les bons plans</h3>
+        <h3 style="font-family: serif;">Les bons plans :</h3>
         <div class="row justify-content-center">
 
           <div class="col-lg-4 col-md-3 mb-4">
           	<div class="card h-100">
-          		<a href="#"><img class="card-img-top" src="http://vps627012.ovh.net/~piscine/piscine/resources/image/bonplan.jpg" alt=""></a>
+          		<a href="#"><img class="card-img-top img-thumbnail" src="http://vps627012.ovh.net/~piscine/piscine/resources/image/adidas.jpg" alt=""></a>
           		<div class="card-body">
-          			<h4 class="card-title">
+          			<h4 class="card-title text-center">
           				<a href="{{route('produit',['CodeProduit'=>6])}}";>Sweat adidas</a>
           			</h4>
           		</div>
@@ -59,9 +59,9 @@
 
           <div class="col-lg-4 col-md-3 mb-4">
           	<div class="card h-100">
-          		<a href="#"><img class="card-img-top" src="http://vps627012.ovh.net/~piscine/piscine/resources/image/bonplan.jpg" alt=""></a>
+          		<a href="#"><img class="card-img-top img-thumbnail" src="http://vps627012.ovh.net/~piscine/piscine/resources/image/timb.jpg" alt=""></a>
           		<div class="card-body">
-          			<h4 class="card-title">
+          			<h4 class="card-title text-center">
           				<a href="{{route('produit',['CodeProduit'=>5])}}";>Timberlands</a>
           			</h4>
           		</div>
@@ -70,9 +70,9 @@
 
           <div class="col-lg-4 col-md-3 mb-4">
           	<div class="card h-100">
-          		<a href="#"><img class="card-img-top" src="http://vps627012.ovh.net/~piscine/piscine/resources/image/bonplan.jpg"></a>
+          		<a href="#"><img class="card-img-top img-thumbnail" src="http://vps627012.ovh.net/~piscine/piscine/resources/image/nikeair.jpg"></a>
           		<div class="card-body">
-          			<h4 class="card-title">
+          			<h4 class="card-title text-center">
           				<a href="{{route('produit',['CodeProduit'=>4])}}";>Nike Air</a>
           			</h4>
           		</div>
