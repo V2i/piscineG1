3fbc267569b3f0ab7ce36da0813cd60d03d71441
V2i <43175508+V2i@users.noreply.github.com>
2019-01-07 14:34:30 +0100

    ajout feature cart

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,16 @@ Route::get('/test_nesrine', function() {
 	return view('home');
 });
 
+Route::get('/pageProduit/{CodeProduit}', 'ProduitController@afficherProduit')->name('produit'); 
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/carte',function() {
+	return view('carte');
+});
+
+Route::get('/basket',function() {
+	return view('basket');
+});
