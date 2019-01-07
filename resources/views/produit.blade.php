e29d6022801797c4980e4fb56c1d01112268b742
nesrinech <barcelona-hoshi@hotmail.fr>
2019-01-08 00:28:18 +0100

    page produit centrée

--- resources/views/produit.blade.php
+++ resources/views/produit.blade.php
@@ -2,21 +2,20 @@
 
 @section('content')
 <div class="container" style="padding-bottom: 20px">
-	
-		<div class="col-lg-9">
-
-          <div class="card mt-4">
-            <img class="card-img-top img-fluid" src="{{ $produit->LienPhotoProduit }}" style="width: 50%;" alt="">
+		<div class="row  justify-content-center">
+          <div class="card md-4">     
+            
+            <img class="img-fluid img-responsive card-img-top img-thumbnail mx-auto d-block" src="{{ $produit->LienPhotoProduit }}" alt="">
+            
             <div class="card-body">
               <u><h2 class="card-title">{{ $produit->NomProduit }}</h2></u>
-              <h4>prix : {{ $produit->PrixProduit }}€</h4>
+              <h4>Prix : {{ $produit->PrixProduit }}€</h4>
               <i><p class="card-text" style="padding-bottom: 10px">Description : {{ $produit->DescriProduit }}</p></i>
-              <button type="button" class="btn btn-info">ajouter au panier</button>
+              <a href="{{ route('basket2',$produit->CodeProduit) }}"><button type="button" class="btn btn-info">ajouter au panier</button></a>
             </div>
           </div>
           <!-- /.card -->
     </div>
-
 </div>
 <div style="padding-bottom: 20px"></div>
 @endsection
